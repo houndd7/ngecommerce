3cac2446106b2ee4955a42152423a36c0270ec80
houndd7 <bustamanted334@gmail.com>
2017-11-30 20:37:16 -0500

    Segundo comentario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoreComponent } from './store/store.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoreComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
